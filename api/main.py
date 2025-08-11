af8fcd7e7f1c302a3d7ba1117c2ce443147d5f2f
janjanpower <janlee0701@gmail.com>
2025-08-12 01:16:41 +0800

    加強N8N4

--- api/main.py
+++ api/main.py
@@ -33,6 +33,7 @@ from api.routes.file_routes import router as file_router
 from api.routes.case_upsert_routes import router as cases_upsert_router
 from api.routes import case_upload_routes
 from api.routes.lawyer_routes import router as lawyer_router
+from api.routes.line_routes import line_router
 
 from api.routes import pending_routes
 from api.routes import case_routes
@@ -48,6 +49,7 @@ app.include_router(case_upload_routes.router)
 app.include_router(lawyer_router)
 app.include_router(pending_routes.router)
 app.include_router(case_routes.router)
+app.include_router(line_router)
 # 掛載 API 路由
 app.include_router(api_routes.router, prefix="/api/auth", tags=["auth"])
 
